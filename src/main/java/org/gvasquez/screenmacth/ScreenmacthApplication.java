9ce2b821302ba11cd5beac76d6cee894ece5a563
Gerardo vasquez <gvsvasquez90@gmail.com>
2024-05-21 14:14:51 -0600

    metodo para realizar peticion

--- src/main/java/org/gvasquez/screenmacth/ScreenmacthApplication.java
+++ src/main/java/org/gvasquez/screenmacth/ScreenmacthApplication.java
@@ -1,13 +1,22 @@
 package org.gvasquez.screenmacth;
 
+import org.gvasquez.screenmacth.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmacthApplication {
+public class ScreenmacthApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmacthApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola desde springboot");
+		var consumoApi = new ConsumoApi();
+		var json= consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=4fc7c187");
+		System.out.println(json);
+	}
 }
