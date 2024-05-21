ce80578c614fb15e58f17dcfa01400d2aba9edc5
Gerardo vasquez <gvsvasquez90@gmail.com>
2024-05-21 13:59:21 -0600

    first commit

---
+++ src/main/java/org/gvasquez/screenmacth/ScreenmacthApplication.java
@@ -0,0 +1,13 @@
+package org.gvasquez.screenmacth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmacthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmacthApplication.class, args);
+	}
+
+}
